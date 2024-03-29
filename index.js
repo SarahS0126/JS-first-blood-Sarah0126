5ed1e885f2a66857d59501a99cf8308f51e29bfb
SarahS0126 <SarahS0126@gmail.com>
2024-03-28 23:50:45 -0500

    add after todo

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const person = {
+const people = [
+  {
     name: "John",
     age: 17,
-  };
-  
-  function verifyAdulthood(personName, personAge) {
-    if (personAge >= 18) return `Welcome, ${personName}!`;
-  
-    return `You are not old enough to enter, ${personName}.`;
-  }
-  
-  const adultMessage = verifyAdulthood(person.name, person.age);
-  
-  console.log(adultMessage);
-  
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
+
+function verifyAdulthood(personName, personAge) {
+  if (personAge >= 18) return `Welcome, ${personName}!`;
+
+  return `You are not old enough to enter, ${personName}.`;
+}
+
+for (
+  let i = 0; i < people.length; i++
+) {
+  const message = verifyAdulthood(people[i].name, people[i].age)
+  console.log(message)
+}
